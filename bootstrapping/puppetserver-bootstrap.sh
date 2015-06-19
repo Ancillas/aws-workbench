262d9acacb0856cbf9fd32ef140c3884bf7eb27d
Greg Baker <gregspammagnet@gmail.com>
2015-06-19 17:59:27 +0000

    Added incomplete puppet server bootstrapping script.

---
+++ bootstrapping/puppetserver-bootstrap.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -e
+set -u
+
+codename=`lsb_release -sc`
+install_deb="puppetlabs-release-pc1-${codename}.deb"
+
+if [ -z "`dpkg -l | grep puppetlabs-release-pc1`" ]; then
+    echo "Downloading ${install_deb}"
+    curl -O https://apt.puppetlabs.com/${install_deb}
+    echo "Installing ${install_deb}"
+    sudo dpkg -i ${install_deb}
+else
+    echo "puppetlabs-release-pc1 repository already installed"
+fi 
+
+echo "Updating apt cache"
+#sudo apt-get update
+
+if [ -z "`dpkg -l | grep puppetserver`" ]; then
+    echo "Installing puppetserver"
+    sudo apt-get -q -y install puppetserver
+else
+    echo "puppetserver package already installed"
+fi
+
+if [ -z "`dpkg -l | grep puppet-agent`" ]; then
+    echo "Installing puppet agent"
+    sudo apt-get -q -y install puppet-agent
+else
+    echo "puppet-agent package already installed"
+fi
+
+if [ ! -f "/etc/profile.d/puppetlabs-bin.sh" ]; then
+sudo tee "/etc/profile.d/puppetlabs-bin.sh" > /dev/null <<'EOF'
+if [ -d "/opt/puppetlabs/bin" ]; then
+    PATH="/opt/puppetlabs/bin:$PATH" 
+fi        
+EOF
+else
+    echo "/etc/profile.d/pupetlabs-bin.sh already exists"
+fi
+sudo chmod 644 /etc/profile.d/puppetlabs-bin.sh
+source /etc/profile.d/puppetlabs-bin.sh 
+
+if [ -z "`grep \"/opt/puppetlabs/bin\" /etc/bash.bashrc`" ]; then
+sudo tee -a "/etc/bash.bashrc" > /dev/null <<'EOF'
+if [ -d "/opt/puppetlabs/bin" ]; then
+    PATH="/opt/puppetlabs/bin:$PATH"
+fi
+EOF
+else
+    echo "/etc/bash.bashrc is already configured to add /opt/puppetlabs/bin to the PATH"
+fi
+
